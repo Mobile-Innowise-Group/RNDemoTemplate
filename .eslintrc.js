6aa5ed0840ea3a011e9408ada441714edcc4810c
Uladzimir Kopanko <uladzimir.kapanko@innowise.com>
2025-01-14 12:21:45 +0100

    feat: setup eslint prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,30 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+	env: {
+		es2021: true
+	},
+	extends: ['plugin:react/recommended', 'prettier'],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 12,
+		sourceType: 'module'
+	},
+	plugins: ['react', 'prettier'],
+	rules: {
+		'prettier/prettier': 'error',
+		'react/jsx-filename-extension': [
+			'warn',
+			{
+				extensions: ['.jsx', '.js']
+			}
+		],
+		'import/prefer-default-export': 'off',
+		'react/state-in-constructor': 'off',
+		'react/static-property-placement': 'off',
+		'react/jsx-props-no-spreading': 'off',
+		'react/prop-types': 'off',
+		'no-param-reassign': 'off',
+		'no-console': 'off'
+	}
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,21 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  // Use single quotes instead of double quotes for strings
   singleQuote: true,
-  trailingComma: 'all',
-};
+  // Add spaces inside object literal braces
+  bracketSpacing: true,
+  // Place closing brackets on a new line for JSX/HTML tags
+  bracketSameLine: false,
+  // Don't add trailing commas (at the end of objects, arrays, etc.)
+  trailingComma: "none",
+  // Avoid parentheses around single arrow function parameters
+  arrowParens: "avoid",
+  // Always add semicolons at the end of statements
+  semi: true,
+  // Set the number of spaces per indentation level (4 spaces)
+  tabWidth: 4,
+  // Use tabs instead of spaces for indentation
+  useTabs: true,
+  // Set the maximum line length to 120 characters before wrapping
+  printWidth: 120
+}
+
